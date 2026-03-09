d1682c7e6efcd6fb68b9aebee94b44b62658ce21
tomat-suppe <slak@itu.dk>
2026-03-09 11:24:28 +0100

    Initial try to get snowflake integrated via npm. Also added npm package dotenv, so that secrets can be made in a .env package

---
+++ Server/Database/DBInit.cjs
@@ -0,0 +1,29 @@
+require('dotenv').config();
+require('snowflake-sdk');
+
+let snowflake = new snowflake-sdk;
+//console.log(process.env);
+console.log(process.env.DBUSER);
+var connection = snowflake-sdk.createConnection({
+  account: "myaccount.us-east-2",
+  username: process.env.DBUSER,
+  password: process.env.DBPASSWORD
+});
+
+
+//const connection = await DuckDBConnection.create();
+//const reader = await connection.run("create table if not exists EventLog (eventName varchar, location varchar, points bigint, logicalTimeStamp bigint, windowsize json)");
+
+//const rows = reader.getRowObjectsJson();
+
+/*console.log(rows);
+
+setInterval(async() => {
+    console.log("data fra DB " + JSON.stringify(receivedData));
+    try { 
+        connection.runAndReadAll("INSERT INTO EventLog SELECT * from read_json(receivedData)");
+    } catch (e) {
+        console.log(`Could not DB data with error ${e}`);
+    }
+}, 5000);
+*/
